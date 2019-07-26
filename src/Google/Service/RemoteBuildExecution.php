393ea746c48ec42bd011641c5a3273aae5bbb843
Google APIs <googleapis-publisher@google.com>
2019-07-26 00:23:29 +0000

    Autogenerated update (2019-07-26)
    Update:
    - remotebuildexecution:v2

--- src/Google/Service/RemoteBuildExecution.php
+++ src/Google/Service/RemoteBuildExecution.php
@@ -79,6 +79,10 @@ class Google_Service_RemoteBuildExecution extends Google_Service
                   'type' => 'string',
                   'required' => true,
                 ),
+                'inlineStdout' => array(
+                  'location' => 'query',
+                  'type' => 'boolean',
+                ),
                 'inlineStderr' => array(
                   'location' => 'query',
                   'type' => 'boolean',
@@ -87,10 +91,6 @@ class Google_Service_RemoteBuildExecution extends Google_Service
                   'location' => 'query',
                   'type' => 'string',
                   'repeated' => true,
-                ),
-                'inlineStdout' => array(
-                  'location' => 'query',
-                  'type' => 'boolean',
                 ),
               ),
             ),'update' => array(
