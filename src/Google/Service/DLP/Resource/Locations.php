a2d4d33fd6e4ce5fd2aa30c99a882acee6e84a56
Google APIs <googleapis-publisher@google.com>
2019-08-29 00:23:08 +0000

    Autogenerated update (2019-08-29)
    Update:
    - cloudasset:v1
    - cloudsearch:v1
    - dlp:v2

--- src/Google/Service/DLP/Resource/Locations.php
+++ src/Google/Service/DLP/Resource/Locations.php
@@ -16,35 +16,30 @@
  */
 
 /**
- * The "infoTypes" collection of methods.
+ * The "locations" collection of methods.
  * Typical usage is:
  *  <code>
  *   $dlpService = new Google_Service_DLP(...);
- *   $infoTypes = $dlpService->infoTypes;
+ *   $locations = $dlpService->locations;
  *  </code>
  */
-class Google_Service_DLP_Resource_InfoTypes extends Google_Service_Resource
+class Google_Service_DLP_Resource_Locations extends Google_Service_Resource
 {
   /**
    * Returns a list of the sensitive information types that the DLP API supports.
    * See https://cloud.google.com/dlp/docs/infotypes-reference to learn more.
-   * (infoTypes.listInfoTypes)
+   * (locations.infoTypes)
    *
+   * @param string $location The geographic location to list info types. Reserved
+   * for future extensions.
+   * @param Google_Service_DLP_GooglePrivacyDlpV2ListInfoTypesRequest $postBody
    * @param array $optParams Optional parameters.
-   *
-   * @opt_param string languageCode Optional BCP-47 language code for localized
-   * infoType friendly names. If omitted, or if localized strings are not
-   * available, en-US strings will be returned.
-   * @opt_param string filter Optional filter to only return infoTypes supported
-   * by certain parts of the API. Defaults to supported_by=INSPECT.
-   * @opt_param string location The geographic location to list info types.
-   * Reserved for future extensions.
    * @return Google_Service_DLP_GooglePrivacyDlpV2ListInfoTypesResponse
    */
-  public function listInfoTypes($optParams = array())
+  public function infoTypes($location, Google_Service_DLP_GooglePrivacyDlpV2ListInfoTypesRequest $postBody, $optParams = array())
   {
-    $params = array();
+    $params = array('location' => $location, 'postBody' => $postBody);
     $params = array_merge($params, $optParams);
-    return $this->call('list', array($params), "Google_Service_DLP_GooglePrivacyDlpV2ListInfoTypesResponse");
+    return $this->call('infoTypes', array($params), "Google_Service_DLP_GooglePrivacyDlpV2ListInfoTypesResponse");
   }
 }
