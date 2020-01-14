203921b5a07b2e9887622ec4d0b4e1fdfce396d8
Google APIs <googleapis-publisher@google.com>
2020-01-14 00:23:51 +0000

    Autogenerated update (2020-01-14)
    Update:
    - cloudbilling:v1

--- src/Google/Service/Cloudbilling.php
+++ src/Google/Service/Cloudbilling.php
@@ -160,13 +160,13 @@ class Google_Service_Cloudbilling extends Google_Service
                   'type' => 'string',
                   'required' => true,
                 ),
-                'pageToken' => array(
-                  'location' => 'query',
-                  'type' => 'string',
-                ),
                 'pageSize' => array(
                   'location' => 'query',
                   'type' => 'integer',
+                ),
+                'pageToken' => array(
+                  'location' => 'query',
+                  'type' => 'string',
                 ),
               ),
             ),
@@ -213,13 +213,13 @@ class Google_Service_Cloudbilling extends Google_Service
               'path' => 'v1/services',
               'httpMethod' => 'GET',
               'parameters' => array(
-                'pageSize' => array(
-                  'location' => 'query',
-                  'type' => 'integer',
-                ),
                 'pageToken' => array(
                   'location' => 'query',
                   'type' => 'string',
+                ),
+                'pageSize' => array(
+                  'location' => 'query',
+                  'type' => 'integer',
                 ),
               ),
             ),
@@ -241,6 +241,10 @@ class Google_Service_Cloudbilling extends Google_Service
                   'type' => 'string',
                   'required' => true,
                 ),
+                'currencyCode' => array(
+                  'location' => 'query',
+                  'type' => 'string',
+                ),
                 'endTime' => array(
                   'location' => 'query',
                   'type' => 'string',
@@ -256,10 +260,6 @@ class Google_Service_Cloudbilling extends Google_Service
                 'pageSize' => array(
                   'location' => 'query',
                   'type' => 'integer',
-                ),
-                'currencyCode' => array(
-                  'location' => 'query',
-                  'type' => 'string',
                 ),
               ),
             ),
