814c64ae4a822ea59d5acf53478ff46795b86e3e
Google APIs <googleapis-publisher@google.com>
2020-04-15 00:24:34 +0000

    Autogenerated update (2020-04-15)
    Update:
    - file:v1
    - prod_tt_sasportal:v1alpha1
    - sasportal:v1alpha1
    - speech:v1

--- src/Google/Service/SASPortalTesting.php
+++ src/Google/Service/SASPortalTesting.php
@@ -436,10 +436,6 @@ class Google_Service_SASPortalTesting extends Google_Service
                   'type' => 'string',
                   'required' => true,
                 ),
-                'filter' => array(
-                  'location' => 'query',
-                  'type' => 'string',
-                ),
                 'pageToken' => array(
                   'location' => 'query',
                   'type' => 'string',
@@ -447,6 +443,10 @@ class Google_Service_SASPortalTesting extends Google_Service
                 'pageSize' => array(
                   'location' => 'query',
                   'type' => 'integer',
+                ),
+                'filter' => array(
+                  'location' => 'query',
+                  'type' => 'string',
                 ),
               ),
             ),'move' => array(
@@ -624,15 +624,15 @@ class Google_Service_SASPortalTesting extends Google_Service
                   'type' => 'string',
                   'required' => true,
                 ),
+                'pageToken' => array(
+                  'location' => 'query',
+                  'type' => 'string',
+                ),
                 'pageSize' => array(
                   'location' => 'query',
                   'type' => 'integer',
                 ),
                 'filter' => array(
-                  'location' => 'query',
-                  'type' => 'string',
-                ),
-                'pageToken' => array(
                   'location' => 'query',
                   'type' => 'string',
                 ),
