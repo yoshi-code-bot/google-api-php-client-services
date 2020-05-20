0a76346a15f1fc145ea3d4a84842e8689f216436
Google APIs <googleapis-publisher@google.com>
2020-05-20 00:24:40 +0000

    Autogenerated update (2020-05-20)
    Update:
    - billingbudgets:v1beta1
    - cloudasset:v1
    - displayvideo:v1
    - gamesConfiguration:v1configuration
    - gamesManagement:v1management
    - remotebuildexecution:v2
    - serviceconsumermanagement:v1
    - servicenetworking:v1
    - serviceusage:v1

--- src/Google/Service/ServiceNetworking.php
+++ src/Google/Service/ServiceNetworking.php
@@ -103,6 +103,10 @@ class Google_Service_ServiceNetworking extends Google_Service
                   'type' => 'string',
                   'required' => true,
                 ),
+                'pageSize' => array(
+                  'location' => 'query',
+                  'type' => 'integer',
+                ),
                 'pageToken' => array(
                   'location' => 'query',
                   'type' => 'string',
@@ -110,10 +114,6 @@ class Google_Service_ServiceNetworking extends Google_Service
                 'filter' => array(
                   'location' => 'query',
                   'type' => 'string',
-                ),
-                'pageSize' => array(
-                  'location' => 'query',
-                  'type' => 'integer',
                 ),
               ),
             ),
