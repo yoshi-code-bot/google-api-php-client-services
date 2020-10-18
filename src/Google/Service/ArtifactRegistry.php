ae03b44ec9c541039dfe8b16d9ea04e45f5eb215
Google APIs <googleapis-publisher@google.com>
2020-10-18 00:26:04 +0000

    Autogenerated update (2020-10-18)
    Update:
    - artifactregistry:v1beta1

--- src/Google/Service/ArtifactRegistry.php
+++ src/Google/Service/ArtifactRegistry.php
@@ -90,13 +90,13 @@ class Google_Service_ArtifactRegistry extends Google_Service
                   'location' => 'query',
                   'type' => 'string',
                 ),
-                'pageSize' => array(
-                  'location' => 'query',
-                  'type' => 'integer',
-                ),
                 'filter' => array(
                   'location' => 'query',
                   'type' => 'string',
+                ),
+                'pageSize' => array(
+                  'location' => 'query',
+                  'type' => 'integer',
                 ),
               ),
             ),
@@ -280,10 +280,6 @@ class Google_Service_ArtifactRegistry extends Google_Service
                   'type' => 'string',
                   'required' => true,
                 ),
-                'filter' => array(
-                  'location' => 'query',
-                  'type' => 'string',
-                ),
                 'pageToken' => array(
                   'location' => 'query',
                   'type' => 'string',
@@ -291,6 +287,10 @@ class Google_Service_ArtifactRegistry extends Google_Service
                 'pageSize' => array(
                   'location' => 'query',
                   'type' => 'integer',
+                ),
+                'filter' => array(
+                  'location' => 'query',
+                  'type' => 'string',
                 ),
               ),
             ),
@@ -332,13 +332,13 @@ class Google_Service_ArtifactRegistry extends Google_Service
                   'type' => 'string',
                   'required' => true,
                 ),
-                'pageSize' => array(
-                  'location' => 'query',
-                  'type' => 'integer',
-                ),
                 'pageToken' => array(
                   'location' => 'query',
                   'type' => 'string',
+                ),
+                'pageSize' => array(
+                  'location' => 'query',
+                  'type' => 'integer',
                 ),
               ),
             ),
@@ -394,7 +394,7 @@ class Google_Service_ArtifactRegistry extends Google_Service
                   'type' => 'string',
                   'required' => true,
                 ),
-                'filter' => array(
+                'pageToken' => array(
                   'location' => 'query',
                   'type' => 'string',
                 ),
@@ -402,7 +402,7 @@ class Google_Service_ArtifactRegistry extends Google_Service
                   'location' => 'query',
                   'type' => 'integer',
                 ),
-                'pageToken' => array(
+                'filter' => array(
                   'location' => 'query',
                   'type' => 'string',
                 ),
