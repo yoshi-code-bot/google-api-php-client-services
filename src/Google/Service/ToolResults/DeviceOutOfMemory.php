f34adabbd4195772a0b9eb319bdd4ee3a138ae3b
Google APIs <googleapis-publisher@google.com>
2020-10-30 00:26:47 +0000

    Autogenerated update (2020-10-30)
    Update:
    - bigquery:v2
    - dialogflow:v2
    - iamcredentials:v1
    - servicecontrol:v2
    - sqladmin:v1beta4
    - toolresults:v1beta3

--- src/Google/Service/ToolResults/DeviceOutOfMemory.php
+++ src/Google/Service/ToolResults/DeviceOutOfMemory.php
@@ -15,6 +15,6 @@
  * the License.
  */
 
-class Google_Service_Dialogflow_GoogleCloudDialogflowV3alpha1RunTestCaseMetadata extends Google_Model
+class Google_Service_ToolResults_DeviceOutOfMemory extends Google_Model
 {
 }
