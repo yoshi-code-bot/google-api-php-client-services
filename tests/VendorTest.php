6692549efd68dfa84e0761e5e733d7aee09b7d50
Brent Shaffer <betterbrent@google.com>
2021-12-01 16:59:48 -0500

    chore: switch master to main (#554)

--- tests/VendorTest.php
+++ tests/VendorTest.php
@@ -32,10 +32,10 @@ class VendorTest extends TestCase
     public function testApiClientVersions($apiClientVersion)
     {
         if (
-            'dev-master' === $apiClientVersion
+            'dev-main' === $apiClientVersion
             && version_compare(PHP_VERSION, '5.6', '<')
         ) {
-            self::markTestSkipped('dev-master of google/api-client can only run on 5.6');
+            self::markTestSkipped('dev-main of google/apiclient can only run on 5.6');
         }
         if ('true' === getenv('SKIP_VENDOR_TESTS')) {
             self::markTestSkipped('Explicitly skipping the vendor tests');
@@ -64,7 +64,7 @@ class VendorTest extends TestCase
     public function provideApiClientVersions()
     {
         return [
-            ['dev-master'],
+            ['dev-main'],
             ['2.7.2'],
         ];
     }
